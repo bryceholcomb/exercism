0a917dbe2cdc99205d1569414dcbd080c6ec9a56
Bryce Holcomb <bryce_holcomb@outlook.com>
2015-02-03 19:05:03 -0700

    finish ruby leap year

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,14 +1,13 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  if (input === input.toUpperCase()) {
+    return "Whoa, chill out!"
+  } else if (input.charAt(input.length - 1) == "?"){
+    return "Sure."
+  } else {
+    return "Whatever."
+  }
 };
 
 module.exports = Bob;
