f1031e1722cb432d8c786eb1630483a7a26f34ce
Bryce Holcomb <bryce_holcomb@outlook.com>
2015-02-12 21:32:08 -0700

    finish javascript bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,10 +1,12 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-  if (input === input.toUpperCase()) {
+  if (input === input.toUpperCase() && input.toUpperCase() !== input.toLowerCase()) {
     return "Whoa, chill out!"
   } else if (input.charAt(input.length - 1) == "?"){
     return "Sure."
+  } else if (input.trim().length === 0) {
+    return "Fine. Be that way!"
   } else {
     return "Whatever."
   }
